863b46898020a4884be18eee0611dea6005d0f1e
Yan Lima Barbosa <yanbr763@gmail.com>
2025-01-04 02:45:00 -0300

    feat: add external dependency  (AnswersRepository) to AnswerQuestionUseCase

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer"
+import { AnswersRepository } from "../repositories/answers-repository"
 
 interface AnswerQuestionUseCaseRequest {
   content: string
@@ -7,8 +8,14 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-  execute({ content, instructorId, questionId, }: AnswerQuestionUseCaseRequest) {
+  constructor(
+    private answersRepository: AnswersRepository
+  ) { }
+
+  async execute({ content, instructorId, questionId, }: AnswerQuestionUseCaseRequest) {
     const answer = new Answer({ authorId: instructorId, content, questionId })
+
+    await this.answersRepository.create(answer)
 
     return answer
   }
